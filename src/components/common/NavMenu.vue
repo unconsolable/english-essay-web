b070b96178e487fc96275465182471d3f2a41f37
unconsolable <chenzhipeng2012@gmail.com>
2021-03-09 21:01:40 +0800

    init SelfLearn.vue

--- src/components/common/NavMenu.vue
+++ src/components/common/NavMenu.vue
@@ -10,7 +10,7 @@
     <!-- <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name">
       {{ item.navItem }}
     </el-menu-item> -->
-    <el-menu-item class="el-icon-menu" key="0" :index="navList[0].name" style="font-size: 16px;">
+    <el-menu-item class="el-icon-house" key="0" :index="navList[0].name" style="font-size: 16px;">
       {{ navList[0].navItem }}
     </el-menu-item>
     <el-menu-item class="el-icon-edit-outline"  key="1" :index="navList[1].name" style="font-size: 16px;">
@@ -21,6 +21,9 @@
     </el-menu-item>
     <el-menu-item class="el-icon-reading" key="3" :index="navList[3].name" style="font-size: 16px;" v-if="this.$store.state.user.role === 'stu'">
       {{ navList[3].navItem }}
+    </el-menu-item>
+    <el-menu-item class="el-icon-medal" key="4" :index="navList[4].name" style="font-size: 16px;" v-if="this.$store.state.user.role === 'stu'">
+      {{ navList[4].navItem }}
     </el-menu-item>
   </el-menu>
 </template>
@@ -34,7 +37,8 @@ export default {
         {name: '/index', navItem: '首页'},
         {name: '/essay', navItem: '作文'},
         {name: '/class', navItem: '班级'},
-        {name: '/selfstudy', navItem: '自主练习'}
+        {name: '/selfstudy', navItem: '自测'},
+        {name: '/selflearn', navItem: '自学'}
       ]
     }
   }
